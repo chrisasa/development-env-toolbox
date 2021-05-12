55be5ca6107f0e501df3e9c6fda2f16c3570d495
Christos <christos.asaridis@gmail.com>
2021-05-12 20:41:40 +0100

    Added comments

--- PowerShell/PROFILE.ps1
+++ PowerShell/PROFILE.ps1
@@ -3,9 +3,11 @@
 # Import-Module oh-my-posh
 # Set-PoshPrompt -Theme paradox
 
+## Terraform
 function Invoke-Terraform { & terraform.exe $args }
 New-Alias -Name tf -Value Invoke-Terraform -Force -Option AllScope
 
+## Docker
 function Invoke-Docker { & docker.exe $args }
 New-Alias -Name dk -Value Invoke-Docker -Force -Option AllScope
 
@@ -28,5 +30,6 @@ function Clear-DockerAll{
 }
 New-Alias -Name dknuke -Value Clear-DockerAll -Force -Option AllScope
 
+## Generic
 function Get-IPExternal { (Invoke-WebRequest ifconfig.me/ip).Content }
 New-Alias -Name getip -Value Get-IPExternal -Force -Option AllScope
