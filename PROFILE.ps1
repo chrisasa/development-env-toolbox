7218523207b71bd64d9895eda7a252f4c981038e
Christos <christos.asaridis@gmail.com>
2021-05-12 13:22:39 +0100

    added $Profile script

---
+++ PROFILE.ps1
@@ -0,0 +1,31 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-PoshPrompt -Theme paradox
+
+function Invoke-Terraform { & terraform.exe $args }
+New-Alias -Name tf -Value Invoke-Terraform -Force -Option AllScope
+
+function Invoke-Docker { & docker.exe $args }
+New-Alias -Name dk -Value Invoke-Docker -Force -Option AllScope
+
+function Clear-DockerAll{
+    docker ps -q | ForEach-Object{
+        $cntId = $_
+    
+        docker kill $cntId
+        docker container rm -f $cntId
+    }
+    
+    docker container prune -f
+    
+    docker images -q | ForEach-Object{
+    
+        $imgId = $_
+    
+        docker image remove -f $imgId
+    }    
+}
+New-Alias -Name dknuke -Value Clear-DockerAll -Force -Option AllScope
+
+function Get-IPExternal { (Invoke-WebRequest ifconfig.me/ip).Content }
+New-Alias -Name getip -Value Get-IPExternal -Force -Option AllScope
